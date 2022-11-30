b8f315884cdf521f84a7bd5b398d5c17903b0ecd
carloso0114 <carlossospinag@gmail.com>
2022-11-29 22:39:31 -0500

    messing around with solutions

--- moveZeros.js
+++ moveZeros.js
@@ -1,4 +1,4 @@
-function moveZeros(arr) {
+/* function moveZeros(arr) {
   let count = 0;
   let result = [];
   for (let number in arr) {
@@ -12,7 +12,17 @@ function moveZeros(arr) {
     result.push(0);
   };
   return result;
+}; */
+
+function moveZeros(arr) {
+  let result = arr.filter(element => element !== 0)
+  let countZeros = arr.filter(v => v === 0);
+  for (let element in countZeros) {
+    result.push(0);
+  }
+  return result;
 };
 
 console.log(moveZeros([1, 2, 0, 1, 0, 1, 0, 3, 0, 1]));
 console.log(moveZeros([false,1,0,1,2,0,1,3,"a"]));
+
